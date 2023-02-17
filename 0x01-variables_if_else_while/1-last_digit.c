50de7a15fde5ee701e46c03e35181d4fac86a412
Benjamin <benjhay100@gmail.com>
2023-02-17 15:45:52 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,23 +1,3 @@
-#include <stdio.h>
-
-/**
- *main - print lowercase a-z
- *Return: Always 0 (Success)
-*/
-
-int main(void)
-{
-	char alpha = 'a';
-
-	while (alpha <= 'z')
-	{
-		putchar(alpha);
-		alpha++;
-	}
-	putchar('\n');
-
-	return (0);
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
