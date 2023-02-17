e873383d96d2e320d30c74ca03bae19941a39479
Benjamin <benjhay100@gmail.com>
2023-02-17 13:40:33 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+i#include <stdio.h>
+/**
+  * main-Entry point
+  *
+  * Return: 0 if successful
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
